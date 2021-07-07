8bc1b1a6896274a9b95d293a51c68567dfa26582
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-07 11:19:00 +0100

    Simplify code

--- src/assignInteractions.ts
+++ src/assignInteractions.ts
@@ -1,9 +1,7 @@
-import makeInteractionLoader from './makeInteractionLoader'
 import { readdirSync } from 'fs'
 import path from 'path'
 
 export default function assignInteractions<T>(thisObj: T, interactionsDir: string): void {
-  const interaction = makeInteractionLoader(interactionsDir)
   const files = readdirSync(interactionsDir)
   for (const file of files) {
     const match = file.match(/(\.ts|\.js|\.tsx|\.jsx)$/)
@@ -11,8 +9,21 @@ export default function assignInteractions<T>(thisObj: T, interactionsDir: strin
       const ext = match[1]
       const name = path.basename(file, ext)
       Object.defineProperty(thisObj, name, {
-        value: interaction(name),
+        value: loadInteraction(name),
       })
+    }
+  }
+
+  function loadInteraction(name: string) {
+    const path = `${interactionsDir}/${name}`
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const interaction = require(path)
+      return interaction[name]
+    } catch (err) {
+      return () => {
+        throw new Error(`No interaction in: ${path}.{ts,js,tsx,jsx}`)
+      }
     }
   }
 }
