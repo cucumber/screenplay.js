5f0c5b2517babc7801f48a992b6c6b9590962a1f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-08-30 17:51:37 +0100

    Longer timeout to pass on Windows

--- test/eventually.test.ts
+++ test/eventually.test.ts
@@ -1,8 +1,8 @@
 import assert = require('assert')
 import eventually, { Condition, EventuallyOptions } from '../src/eventually'
 
-const options: EventuallyOptions = {
-  timeout: 10,
+const options: Required<EventuallyOptions> = {
+  timeout: 100,
   interval: 2,
 }
 
@@ -41,8 +41,8 @@ describe('eventually', () => {
     })
 
     it('rejects with a timeout error when the condition never settles', async () => {
-      const condition: Condition = () => new Promise((resolve) => setTimeout(resolve, 200))
-      await assert.rejects(eventually(condition, options), { message: 'Timeout after 10ms' })
+      const condition: Condition = () => new Promise((resolve) => setTimeout(resolve, options.timeout * 2))
+      await assert.rejects(eventually(condition, options), { message: `Timeout after ${options.timeout}ms` })
     })
   })
 
@@ -80,8 +80,8 @@ describe('eventually', () => {
     })
 
     it('rejects with a timeout error when the condition never settles', async () => {
-      const condition: Condition = () => new Promise((resolve) => setTimeout(resolve, 200))
-      await assert.rejects(eventually(condition, options), { message: 'Timeout after 10ms' })
+      const condition: Condition = () => new Promise((resolve) => setTimeout(resolve, options.timeout * 2))
+      await assert.rejects(eventually(condition, options), { message: `Timeout after ${options.timeout}ms` })
     })
   })
 })
