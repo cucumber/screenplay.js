e2289d499d8666ae3913fc3966adde1193ac96d8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-10-10 13:46:48 +0200

    Move examples

--- features/support/tasks/dom/interactions/types.ts
+++
@@ -1,5 +0,0 @@
-import { Action, PromiseAction } from '../../../../../src'
-import { Message } from '../../../../src/types'
-
-export type ClickShoutButton = () => PromiseAction
-export type TypeMessage = (message: Message) => Action
